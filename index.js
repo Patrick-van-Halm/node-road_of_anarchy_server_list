b9d45ef35baca1dcc816d16db01e7014ead6a587
Patrick van Halm <pvanhalm00@gmail.com>
2022-09-21 10:34:58 +0200

    Add deleting servers

--- index.js
+++ index.js
@@ -10,18 +10,35 @@ const client = createClient({
 await client.connect();
 
 app.post("/servers", async (req, res) => {
+    console.log("POST\n" + req.body);
     if(!req.body.ip || !req.body.port) return res.status(400) && res.send("Invalid data");
     let servers = JSON.parse(await client.get("servers")) || [];
     let server = servers.find(s => s.ip === req.body.ip);
     if(server && server.port === req.body.port) return res.status(200) && res.send("OK");
     if(server) server.port = req.body.port;
-    else servers.push({ip: req.body.ip, port: req.body.port});
+    else servers.push({ip: req.body.ip, port: parseInt(req.body.port)});
     await client.set("servers", JSON.stringify(servers));
     res.status(201);
     res.send("Inserted");
 })
 
+app.delete("/servers", async (req, res) => {
+    console.log("DELETE\n" + req.query);
+    if(!req.query.ip || !req.query.port) return res.status(400) && res.send("Invalid data");
+    let servers = JSON.parse(await client.get("servers"));
+    if(!servers) return res.status(200) && res.send("OK");
+
+    let i = servers.findIndex(s => s.ip === req.query.ip && s.port === parseInt(req.query.port));
+    if(i < 0) return res.status(200) && res.send("OK");
+
+    servers.splice(i, 1);
+    await client.set("servers", JSON.stringify(servers));
+    res.status(200);
+    res.send("OK");
+})
+
 app.get("/servers", async (req, res) => {
+    console.log("GET");
     res.json(JSON.parse(await client.get("servers")));
 })
 
